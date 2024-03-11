70618e048662d124b7b5c1a1bf3fe0391171d3b3
luckyian <ian.wren81@gmail.com>
2024-03-10 19:35:00 -0700

    Edited page to add bios, pictures, and makesure spacing is correct

--- src/pages/Bios.js
+++ src/pages/Bios.js
@@ -20,7 +20,12 @@ function Bios() {
 
         </Card.Title>
 
-        
+        <Card.Img src="./assets/Cary.jpg" className="p-2" alt="Cary Webb"></Card.Img>
+        <br></br>
+        <br></br>
+        <h3>CEO of Black Cat Technology with more than two decades of business experience working for both private and public companies, etc.  A little bit of filler
+          to check text sizing and debug any issues related to text.
+        </h3>
 
       </Card.Body>
       <br></br>
@@ -36,6 +41,13 @@ function Bios() {
         <h2>Cyle Webb</h2>
 
         </Card.Title>
+        <Card.Img src="./assets/Cyle.png" className="p-2" alt="Cyle Webb"></Card.Img>
+        <br></br>
+        <br></br>
+        <h3>CFO of Black Cat Technology and our lead design and innovation manager.  He has been a part of various creative and innovative projects that
+          gives us unique perspectives and creative solutions to this modern and evolving world.  More text to test text sizing and debug any issues involving 
+          spacing.
+        </h3>
 
         
 
@@ -53,6 +65,14 @@ function Bios() {
         <h2>Kevin Wren</h2>
 
         </Card.Title>
+        <Card.Img src="./assets/KevinW.png" className="p-2" alt="Kevin Wren"></Card.Img>
+        <br></br>
+        <br></br>
+        <h3>The head of our marketing division, Kevin has more the 15 years experience in marketing.  He boasts a Bachlors in Strategic Communication, and has a Masters in Public Affairs.  He
+          has led several social justice media campaigns and has been invited to speak before both the Washington State and California State Legislatures.  He was 
+          most recently personally invited to the 2024 State of the Union in Washington DC.  Some of his former employers include Publicis Consultants, Waggener Edstrom, and 
+          Porter Novelli.  His insight helps us navigate the current state of business and develop solutions to growing businesses.
+        </h3>
 
         
 
@@ -70,6 +90,15 @@ function Bios() {
         <h2>Ian Wren</h2>
 
         </Card.Title>
+        <Card.Img src="./assets/Ian.jpg" className="p-2" alt="Ian Wren"></Card.Img>
+        <br></br>
+        <br></br>
+        <h3>Ian is our CTO and devlops and designs web based products for our clients.  He has several years designing and lauching websites for various businesses, and 
+          has worked on several mobile applications available in both the Apple Store and Android Play Store.  He is also CompTIA certified Security+ and CySA+ adding a layer
+          of understanding and experience into the ever evolving world of Cyber Security.  He works with both our clients and the internal teams to develop, design, and troubleshoot
+          everything internet.
+
+        </h3>
 
         
 
